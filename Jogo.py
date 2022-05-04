352a1d625d144baf67ede40f6e92235c1bca0152
GabrielGalazzi <gabrielog@al.insper.edu.br>
2022-05-04 14:39:01 -0300

    def normaliza pronta

--- Jogo.py
+++ Jogo.py
@@ -1,1 +1,7 @@
-print('ola')
+def normaliza(dic):
+    dicpais = {}
+    for continente in dic:
+        for pais in dic[continente]:
+            dicpais[pais] = dic[continente][pais]
+            dicpais[pais]['Continente'] = str(continente)
+    return dicpais
